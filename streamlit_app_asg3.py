7be5212f793aecc50d7f0d07648125e6a45c2373
GeraldKoh <141351058+GeraldKoh@users.noreply.github.com>
2023-08-07 04:12:03 +0800

    Update streamlit_app_asg3.py

--- streamlit_app_asg3.py
+++ streamlit_app_asg3.py
@@ -49,7 +49,7 @@ with tab5:
 
     menu_items = maintable["MENU_ITEM_NAME"].unique()
     menu_item_mapping = {item: i for i, item in enumerate(menu_items)}
-    menu_item_labels = list(menu_mapping.keys())
+    menu_item_labels = list(menu_item_mapping.keys())
 
     def get_city():
         city = st.selectbox('Select a City', city_labels)
