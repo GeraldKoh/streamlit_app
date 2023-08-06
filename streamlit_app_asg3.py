32dab3a8271f9aecfa707bb86386852c375b5d11
GeraldKoh <141351058+GeraldKoh@users.noreply.github.com>
2023-08-07 01:53:01 +0800

    Update streamlit_app_asg3.py

--- streamlit_app_asg3.py
+++ streamlit_app_asg3.py
@@ -9,4 +9,12 @@ st.set_page_config(page_title='INVEMP Tasty Bytes Group 5', page_icon='🍖🍕�
 st.sidebar.title("INVEMP: Inventory/Warehouse Management & Prediction on Sales per Menu Item")
 st.sidebar.markdown("This web app allows you to explore the internal inventory of Tasty Bytes. You can explore these functions in the web app (Description of Page)")
 
-tab1, tab2, tab3, tab4, tab5 = st.tabs(['Prediction A', 'Prediction B', 'Shift_Sales_Aust', 'Prediction D', 'Prediction E'])
+tab1, tab2, tab3, tab4, tab5 = st.tabs(['Prediction A', 'Prediction B', 'Sales Prediction Australia', 'Prediction D', 'Prediction E'])
+
+with tab3:
+    # Define the app title and favicon
+    st.title('Shift Sales of Cities in Australia') 
+    
+    st.subheader('Predict')
+    st.markdown("This tab allows predictions on the price of a listing based on the neighbourhood and room type. The model used is a Random Forest Regressor trained on the Airbnb Singapore listings dataset.")
+    st.write('Choose a neighborhood group, neighborhood, and room type to get the predicted average price.')
