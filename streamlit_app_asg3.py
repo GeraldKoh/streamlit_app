6b414693db4e867693b8014afbbe40531fc48aa7
GeraldKoh <141351058+GeraldKoh@users.noreply.github.com>
2023-08-10 04:48:50 +0800

    Update streamlit_app_asg3.py

--- streamlit_app_asg3.py
+++ streamlit_app_asg3.py
@@ -140,6 +140,7 @@ with tab5:
         prediction = xgb_final.predict(input_df)
         total_sales = prediction.sum()
         st.write("Total Shift Sales Before Revamp:", total_sales)
+    st.write('Click the button below only if the YEAR is set to 2022 to see the increase!')
     if st.button('Predict Shift Sales After Menu Optimization'):
         city_int = match_city(city_input)
         shiftid_int = match_shiftid(shiftid_input)
@@ -168,7 +169,6 @@ with tab5:
                                                          'TRUCK_BRAND_NAME_Tasty Tibs','TRUCK_BRAND_NAME_Cheeky Greek',"TRUCK_BRAND_NAME_Nani's Kitchen",'TRUCK_BRAND_NAME_The Mega Melt','TRUCK_BRAND_NAME_Revenge of the Curds','TRUCK_BRAND_NAME_The Mac Shack','TRUCK_BRAND_NAME_Not the Wurst Hot Dogs',"TRUCK_BRAND_NAME_Guac n' Roll"])
         projected_prediction = xgb_final.predict(new_input_df)
         projected_total_sales = projected_prediction.sum()
-        st.write('Click the button below only if the YEAR is set to 2022 to see the increase!')
         st.write("Projected Total Shift Sales After Menu Optimization:", projected_total_sales)
 
 
