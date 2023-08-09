9b7176e98f76a6f31fe758ad8306699ee889fae5
GeraldKoh <141351058+GeraldKoh@users.noreply.github.com>
2023-08-09 15:43:12 +0800

    Update streamlit_app_asg3.py

--- streamlit_app_asg3.py
+++ streamlit_app_asg3.py
@@ -154,8 +154,8 @@ with tab5:
                                          'TOT_PRECIPITATION_IN',
                                          'TOT_SNOWFALL_IN', 'SHIFT_NUMBER', 'MENU_ITEM_NAME', 
                                          'ITEM_CATEGORY','ITEM_SUBCATEGORY','TRUCK_BRAND_NAME','YEAR'])
+        st.write(input_df)
         # input_data = [[shiftid_input, city_input, AVG_TEMPERATURE_AIR_2M_F, AVG_WIND_SPEED_100M_MPH, TOT_PRECIPITATION_IN, TOT_SNOWFALL_IN, SHIFT_NUMBER, MENU_ITEM_NAME, ITEM_CATEGORY, ITEM_SUBCATEGORY, TRUCK_BRAND_NAME,YEAR]]
-        # st.write(input_data)
         # input_df = pd.DataFrame(input_data, columns=['SHIFT_ID','CITY','AVG_TEMPERATURE_AIR_2M_F','AVG_WIND_SPEED_100M_MPH','TOT_PRECIPITATION_IN','TOT_SNOWFALL_IN','SHIFT_NUMBER', 'MENU_ITEM_NAME', 'ITEM_CATEGORY','ITEM_SUBCATEGORY','TRUCK_BRAND_NAME','YEAR'])
         prediction = xgb_final.predict(input_df)
         total_ss = 0
